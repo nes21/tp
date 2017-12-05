44cbdcb4a7984335875f2950d32650bff80b1e4b
nes21 <33746113+nes21@users.noreply.github.com>
2017-12-05 17:42:13 +0100

    Update bdd.php

--- bdd.php
+++ bdd.php
@@ -1,7 +1,7 @@
 <?php
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=botrallu;charset=utf8', 'root', '');
+	$bdd = new PDO('mysql:host=localhost;dbname=botrallu;charset=utf8', 'root', 'test');
 }
 catch(Exception $e)
 {
